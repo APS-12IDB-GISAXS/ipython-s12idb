b36ee68e67d66ff2e8fe6ee704d77d9db52fe53e
S12 Service Acct <s12idb@eggplant.xray.aps.anl.gov>
2018-01-09 10:15:58 -0600

    basic config, enough to get started

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -10,5 +10,5 @@ aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
 
 ## Beam Monitor Counts
 #bs_bm2 = EpicsSignalRO('BL14B:Det:BM2', name='bs_bm2')
-noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
-scaler = EpicsScaler('xxx:scaler1', name='scaler')
+calc1 = EpicsSignalRO('12idb:userCalc1', name='calc1')
+scaler = EpicsScaler('12idb:scaler1', name='scaler')
