a3d339edc97be8b30da0a4aa86bd51a655f4beed
jemian <prjemian@gmail.com>
2018-01-17 13:54:54 -0600

    signals & imports

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -1,14 +1,7 @@
 print(__file__)
 
-from ophyd import (EpicsScaler, EpicsSignal, EpicsSignalRO,
-                   Device, DeviceStatus)
-from ophyd import Component as Cpt
-
-import time
+"""various detectors and other signals"""
 
 aps_current = EpicsSignalRO("S:SRcurrentAI", name="aps_current")
-
-## Beam Monitor Counts
-#bs_bm2 = EpicsSignalRO('BL14B:Det:BM2', name='bs_bm2')
-calc1 = EpicsSignalRO('12idb:userCalc1', name='calc1')
+userCalcs_12idb = userCalcsDevice("12idb:", name="userCalcs_12idb")
 scaler = EpicsScaler('12idb:scaler1', name='scaler')
