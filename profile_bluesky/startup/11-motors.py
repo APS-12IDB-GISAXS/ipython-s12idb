e01b8acc31109e20b577bd8ea85d7789769f613c
jemian <prjemian@gmail.com>
2018-01-17 13:33:58 -0600

    refactor

--- profile_bluesky/startup/11-motors.py
+++ profile_bluesky/startup/11-motors.py
@@ -1,17 +1,16 @@
 print(__file__)
-from ophyd import (PVPositioner, EpicsMotor, EpicsSignal, EpicsSignalRO,
-                   PVPositionerPC, Device)
-from ophyd import Component as Cpt
+
+"""motors, stages, positioners, ..."""
 
 # m1 = MyEpicsMotorWithDial('12idb:m1', name='m1')
 
-chi = EpicsMotor('12idb:m1', name='chi')
-crl_z = EpicsMotor('12idb:m2', name='crl_z')
-pitch = EpicsMotor('12idb:m3', name='pitch')
-yaw = EpicsMotor('12idb:m4', name='yaw')
-theta = EpicsMotor('12idb:m5', name='theta')
+chi      = EpicsMotor('12idb:m1', name='chi')
+crl_z    = EpicsMotor('12idb:m2', name='crl_z')
+pitch    = EpicsMotor('12idb:m3', name='pitch')
+yaw      = EpicsMotor('12idb:m4', name='yaw')
+theta    = EpicsMotor('12idb:m5', name='theta')
 sample_v = EpicsMotor('12idb:m6', name='sample_v')
 sample_h = EpicsMotor('12idb:m7', name='sample_h')
-phi = EpicsMotor('12idb:m8', name='phi')
+phi      = EpicsMotor('12idb:m8', name='phi')
 
 append_wa_motor_list(chi, crl_z, pitch, yaw, theta, sample_h, sample_v, phi)
