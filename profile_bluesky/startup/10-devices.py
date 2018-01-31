9c72eabe6bfad3aa69b092e97e7448d0ea6c12c8
jemian <prjemian@gmail.com>
2018-01-31 17:26:26 -0600

    remove unused

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -7,10 +7,3 @@ from ophyd import EpicsMotor, EpicsScaler
 from ophyd import EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV
 from ophyd import PVPositioner, PVPositionerPC
 from APS_BlueSky_tools.devices import userCalcsDevice
-
-class MotorDialValues(Device):
-    value = Component(EpicsSignalRO, ".DRBV")
-    setpoint = Component(EpicsSignal, ".DVAL")
-
-class MyEpicsMotorWithDial(EpicsMotor):
-    dial = Component(MotorDialValues, "")
